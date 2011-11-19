f15a91d96684ef30cbf0ae13f6f86dc7a470f6d9
Takashi SASAKI <takashi316@gmail.com>
2011-11-19 10:09:40 +0900

    Model.py was almost completely rewritten. EquivalenceModel and ExclusionModel were the significant.

--- Resources/obomb/Model.py
+++ Resources/obomb/Model.py
@@ -1,5 +1,8 @@
-from google.appengine.ext import db
-from google.appengine.api import users
+from google.appengine.ext import db, blobstore
+import google.appengine.ext.db.polymodel
+
+#from multiprocessing.managers import SyncManager
+#from google.appengine.api import users
 
 class IriModel(db.Model):
     """
@@ -36,7 +39,7 @@ class IriModel(db.Model):
     def GetFragment(self):
         return self.fragment
 
-class SyncModel(db.polymodel.PolyModel):
+class SyncModel(google.appengine.ext.db.polymodel.PolyModel):
     """This class models hub style data synchronizing system.
     idNumber is signed integer.
     zero indicates invalid ID number.
@@ -44,7 +47,7 @@ class SyncModel(db.polymodel.PolyModel):
     positive idNumber is assigned by obomb web service and globally unique.
     None owner indicates that the instance is well-known and authorized by standards outside.
     """
-    idNumber = db.IntegerProperty()
+    uuid = db.StringProperty()
     owner = db.UserProperty()
     lockBegins = db.DateTimeProperty()
     lockEnds = db.DateTimeProperty()
@@ -54,28 +57,42 @@ class SyncModel(db.polymodel.PolyModel):
     #lastStableDurationBegins = db.DateTimeProperty()
     #lastStableDurationEnds = db.DateTimeProperty()
 
-class VerseModel(SyncModel):
+class ContentModel(SyncModel):
     """Properties in this model never relies on other information
     except the data that sourceUri points to.
     SHA512 is the best on 64bit machine for now.
     """
     sourceUri = db.ReferenceProperty(IriModel)
-    sha512 = db.ByteStringProperty()
-    retrievedString = db.StringProperty()
-    retrievedBlob = db.BlobProperty()
-    retrievedText = db.TextProperty()
+    #sha512 = db.ByteStringProperty()
+    contentString = db.StringProperty() #up to 500 characters
+    contentByteString = db.ByteStringProperty() #Like StringProperty, except the value is not encoded in any way. The bytes are stored literally.
+    contentText = db.TextProperty() #can store more than 500 characters. not indexed and cannot be used in filters or sort orders.
+    
+    #Binary data, as a byte string. This is a subclass of the built-in str type.
+    #Blob properties are not indexed, and cannot be used in filters or sort orders.
+    #Blob is for binary data, such as images. It takes a str value, 
+    #but this value is stored as a byte string and is not encoded as text. 
+    #Use a Text instance for large text data.
+    contentBlob = db.BlobProperty()
 
-class PivotModel(db.polymodel.PolyModel):
+    contentBlobReference = blobstore.BlobReferenceProperty()
+
+class EquivalenceModel(SyncModel):
     """Pivot is a set of verses.
     It represents an abstraction or concept.
     pivotId is primary idNumber that is one of verses list. 
     """
-    idNumber = db.IntegerProperty()
-    verseItems = db.ListProperty(db.IntegerProperty)
+    #equivalenceId = db.IntegerProperty()
+    equivalents = db.ListProperty(db.Key) #db.Key refers an instance of ContentModel
+    primaryVariant = db.ReferenceProperty(ContentModel, collection_name="primaryvariant_set")
+    alphabeticVariant = db.ReferenceProperty(ContentModel, collection_name="alphabeticvariant_set")
+    rubyVariant = db.ReferenceProperty(ContentModel, collection_name="rubyvariant_set")
 
-class KnotModel(PivotModel):
-    parentKnot = db.IntegerProperty()   
-    metadataIris = db.ListProperty(db.Key)
+class ExclusionModel(SyncModel):
+    #exclusionId = db.IntegerProperty()
+    exclusives = db.ListProperty(db.Key) # db.Key refers an instance of EquivalenceModel 
+    #parentKnot = db.IntegerProperty()   
+    #metadataIris = db.ListProperty(db.Key)
     #verse = db.Reference(VerseModel)
     #idnumber = db.IntegerProperty()
     #identityUri = db.ReferenceProperty(IriModel)
@@ -83,62 +100,21 @@ class KnotModel(PivotModel):
     #createdDateTime = db.DateTimeProperty()
     #modifiedDateTime = db.DateTimeProperty()
 
-class PredicationModel(db.Model):
-    predicationId = db.IntegerProperty()
-    arc = db.IntegerProperty()
-    node = db.IntegerProperty()
+class PredicationModel(SyncModel):
+    """A predication is a pair of two terms."""
+    #predicationId = db.IntegerProperty()
+    arc = db.ReferenceProperty(ContentModel, collection_name="arc_set")
+    node = db.ReferenceProperty(ContentModel, collection_name="node_set")
 
-class MetadataModel(db.Model):
-    metadataId = db.IntegerProperty()
-    predications = db.ListProperty(db.IntegerProperty)
+
+class MetadataModel(SyncModel):
+    """A metadata is a set of predications"""
+    #metadataId = db.IntegerProperty()
+    predications = db.ListProperty(db.Key) # db.Key refers an instance of PredicationModel
 
 ###############################################################################
 ##################### THE FOLLOWING MODELS ARE OBSOLETED. #####################
 ###############################################################################
-
-class DescriptiveMetadataModel(db.Expando):
-    """Descriptive metadata describes resource for purposes such as discovery and identification. 
-    It can include elements such as title, abstract, author, and keywords. (NISO, ISBN 1-880124-62-9)
-    In obomb, this model is indented to hold most generic information.
-    They are self descriptive, independent from environment and media.
-    """
-    idnumber = db.IntegerProperty()
-    title = db.StringProperty()
-    keywords = db.StringListProperty()
-    authors = db.StringListProperty()
-
-class TechnicalMetadataModel(db.Expando): 
-    """Technical metadata focuses on how a digital object was created, its format, 
-    format-specific technical characteristics, storage and location, etc. 
-    Accurate technical metadata helps a repository manage digital 
-    objects over time and keep them usable. 
-    (http://hul.harvard.edu/ois/digproj/metadata-standards.html)
-    """
-    idnumber = db.IntegerProperty()
-    messageDigest = db.StringProperty()
-    size = db.IntegerProperty()
-    owner = db.StringProperty()
-    createdDateTime = db.DateTimeProperty()
-    modifiedDateTime = db.DateTimeProperty()    
-    url = db.URLProperty()
-    geo = db.GeoPtProperty()
-
-class AdministrativeMetadataModel(db.Expando):
-    idnumber = db.IntegerProperty()
-    controlledBy = db.UserProperty()
-    lastSeenDateTime = db.DateTimeProperty()
-    lastSeenBy = db.StringProperty()
-    lastSeenFrom = db.StringProperty()
-    incomingToken = db.StringProperty()
-    outgoingToken = db.StringProperty()
-    allowedUsers = db.ListProperty(users.User)
-
-class StructuralMetadataModel(db.Expando):
-    """Structural metadata indicates how compound objects are pot together,
-    for example, how pages are ordered to form chapters. (NISO ISBN 1-880124-62-9)
-    METS (Metadata Encoding & Transmission Standard) includes structural metadata standard.
-    """
-    idnumber = db.IntegerProperty()
 
 #class MetadataSuiteModel(db.Expando):
 #    idnumber = db.IntegerProperty()
@@ -198,11 +174,11 @@ class StructuralMetadataModel(db.Expando):
 import unittest
 class Test(unittest.TestCase):
     def setUp(self):
-        descriptive_metadata = DescriptiveMetadataModel()
-        descriptive_metadata.authors = ["Takashi SASAKI", "Nanashi no Gombei"]
-        descriptive_metadata.keywords = ["Sunday", "Monday"]
-        descriptive_metadata.title = "Sunny day"
-        descriptive_metadata.put()
+        metadata = MetadataModel()
+        metadata.authors = ["Takashi SASAKI", "Nanashi no Gombei"]
+        metadata.keywords = ["Sunday", "Monday"]
+        metadata.title = "Sunny day"
+        metadata.put()
 
     def tearDown(self):
         pass
