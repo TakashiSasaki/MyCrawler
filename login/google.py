f8167c72ad8487b5fcf4acfe794519ae5c98b11f
unknown <Takashi@vpcz22.(none)>
2012-01-03 16:51:33 +0900

    login page with google account was implemented.

--- login/google.py
+++ login/google.py
@@ -6,17 +6,52 @@ Created on 2012/01/03
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from gaesessions import get_current_session
-from models import SessionModel
+from models import SessionKind
 import os, uuid
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from models.ObombUserKind import ObombUserKind
+
 class LoginGoogle(webapp.RequestHandler):
     def get(self):
         current_session = get_current_session()
-        if current_session[SessionModel.SESSION_ID_KEY] is None:
-            current_session[SessionModel.SESSION_ID_KEY] = uuid.uuid1().get_hex()
-        self.response.out.write("<html><body>")
-        self.response.out.write("<p>Currently not implemented</p>")
-        self.response.out.write("<p>The session ID is %s</p>" % current_session[SessionModel.SESSION_ID_KEY])
-        self.response.out.write("</body></html>")
+        if current_session.get(SessionKind.SESSION_ID_KEY) is None:
+            current_session[SessionKind.SESSION_ID_KEY] = uuid.uuid1().get_hex()
+        session_id = current_session.get(SessionKind.SESSION_ID_KEY)
+
+        session_kind_gql = SessionKind.SessionKind.gql("WHERE sessionId = :1", [session_id])
+        session_entity = session_kind_gql.get()
+        if session_entity is not None:
+            session_entity = SessionKind()
+            session_entity.sessionId = session_id
+            session_entity.put()
+            
+        current_user = users.get_current_user()
+        if current_user is not None:
+            google_user_identifier = current_user.user_id()
+            google_user_nickname = current_user.nickname()
+            google_user_email = current_user.email()
+            obomb_user_kind_gql = ObombUserKind.gql("WHERE googleUserIdentifier = :1", [current_user.user_id()])
+            obomb_user_entity = obomb_user_kind_gql.get()
+            if obomb_user_entity is None:
+                obomb_user_entity = ObombUserKind()
+                obomb_user_entity.googleUserIdentifier = current_user.user_id()
+                obomb_user_entity.put()
+        else:
+            google_user_identifier = None
+            google_user_nickname = None
+            google_user_email = None
+
+        template_values = {
+            'session_id' : current_session.get(SessionKind.SESSION_ID_KEY),
+            'google_login_url' : users.create_login_url("/login/google"),
+            'google_logout_url' : users.create_logout_url("/login/google"),
+            'google_user_nickname' : google_user_nickname,
+            'google_user_email' : google_user_email,
+            'google_user_identifier': google_user_identifier
+            }
+        path = os.path.join(os.path.dirname(__file__), 'google.html')
+        self.response.out.write(template.render(path, template_values))
     
     def post(self):
         self.get()
--- models/ObombUserKind.py
+++ models/ObombUserKind.py
@@ -4,7 +4,7 @@ Created on 2012/01/03
 @author: Takashi
 '''
 from google.appengine.ext import db
-class UserModel(db.Model):
+class ObombUserKind(db.Model):
     #__slot__ = ["googleUserIdentifier", "mixiUserIdentifier", "twitterUserIdentifier", "YahooJapanUserIdentifier"]
     googleUserIdentifier = db.StringProperty()
     twitterUserIdentifier = db.StringProperty()
