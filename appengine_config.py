2b34647722db22196487924b9e5bd1d077646e85
Takashi SASAKI <takashi316@gmail.com>
2012-01-03 17:52:46 +0900

    django 1.2 is explicitly specified

--- appengine_config.py
+++ appengine_config.py
@@ -2,3 +2,5 @@ from gaesessions import SessionMiddleware
 def webapp_add_wsgi_middleware(app):
     app = SessionMiddleware(app, cookie_key="obomb_web_gae_sessions_key_98937181609869846319751975105710")
     return app
+from google.appengine.dist import use_library
+use_library('django', '1.2')
