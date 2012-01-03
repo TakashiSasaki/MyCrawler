df249b3d261794ef55afe376983136a1a3abeffb
unknown <Takashi@vpcz22.(none)>
2012-01-03 14:38:59 +0900

    /login/google works as a skelton.

---
+++ login/twitter.py
@@ -0,0 +1,22 @@
+'''
+Created on 2012/01/03
+
+@author: Takashi
+'''
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class LoginTwitter(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("<html><body>")
+        self.response.out.write("<p>currently not implemented</p>")
+        self.response.out.write("</body></html>")
+    
+    def post(self):
+        self.get()
+
+if __name__ == "__main__":
+    application = webapp.WSGIApplication(
+                                     [('/login/google', LoginTwitter)],
+                                     debug=True)
+    run_wsgi_app(application)
