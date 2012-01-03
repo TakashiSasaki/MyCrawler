66260845a9d03d3abcf1e8050199d4c97917dff6
Takashi SASAKI <takashi316@gmail.com>
2012-01-03 18:41:10 +0900

    session clearing page is now /logout

--- logout.py
+++ logout.py
@@ -1,15 +1,17 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from gaesessions import get_current_session
-from models import SessionModel
+from models import SessionKind
 import os, uuid
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from models.ObombUserKind import ObombUserKind
 
 class LogoutRequestHandler(webapp.RequestHandler):
     def get(self):
         current_session = get_current_session()
         template_values = {
-            'session_id' : current_session.get(SessionModel.SESSION_ID_KEY),
+            'session_id' : current_session.get(SessionKind.SESSION_ID_KEY),
             }
         current_session.clear()
         path = os.path.join(os.path.dirname(__file__), 'logout.html')
