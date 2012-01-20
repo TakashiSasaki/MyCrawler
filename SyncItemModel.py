99a2e0c224f51cfc9b703ebe40e4d78c47fa9288
unknown <Takashi@vpcz22.(none)>
2012-01-20 17:40:02 +0900

    itemId and owner are now references.

--- SyncItemModel.py
+++ SyncItemModel.py
@@ -4,9 +4,10 @@ Created on 2012/01/19
 @author: Takashi
 '''
 from google.appengine.ext.db.polymodel import PolyModel
-from google.appengine.ext.db import DateTimeProperty, IntegerProperty, EmailProperty
+from google.appengine.ext.db import DateTimeProperty, IntegerProperty, EmailProperty, \
+    ReferenceProperty
 
-class SyncModel(PolyModel):
+class SyncItemModel(PolyModel):
     """This class models hub style data synchronizing system.
     idNumber is signed integer.    
     zero indicates invalid ID number.
@@ -14,11 +15,11 @@ class SyncModel(PolyModel):
     positive idNumber is assigned by obomb web service and globally unique.
     None owner indicates that the instance is well-known and authorized by standards outside.
     """
-    syncId = IntegerProperty()
-    owner = EmailProperty()
-    lockBegins = DateTimeProperty()
-    lockEnds = DateTimeProperty()
-    versionNumber = IntegerProperty()
+    itemId = ReferenceProperty(required=True)
+    owner = ReferenceProperty(required=True)
+    lockBegins = DateTimeProperty(required=True)
+    lockEnds = DateTimeProperty(required=True)
+    versionNumber = IntegerProperty(required=True)
     #lastUploaded = db.DateTimeProperty()
     #lastDownloaded = db.DateTimeProperty()
     #lastStableDurationBegins = db.DateTimeProperty()
