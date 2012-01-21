8e55dcec8f6970bdc96042dd2b2effc348fc456b
Takashi SASAKI <takashi316@gmail.com>
2012-01-21 19:43:03 +0900

    Identifier takes string as its identifier instead of url.

--- Identifier.py
+++ Identifier.py
@@ -7,10 +7,10 @@
 from google.appengine.ext.db import URLProperty, StringListProperty, \
     ListProperty, BooleanProperty, ReferenceProperty, StringProperty
 from google.appengine.api.datastore import Key
-from unittest.case import TestCase
 from GaeAdopter import Initialize
 from libobomb.Uuid import GetBase32Uuid
 from libobomb.VersionedItemModel import VersionedItemModel
+from unittest.case import TestCase
 
 class IdentifierModel(VersionedItemModel):
     identifierString = ReferenceProperty(required=True)
@@ -38,7 +38,7 @@ class Test(TestCase):
     def testSomething(self):
         Initialize("obomb")
         identifier = IdentifierModel()
-        identifier.identifierUri = "urn:uuid:" + GetBase32Uuid()
+        identifier.identifierString = "urn:uuid:" + GetBase32Uuid()
         identifier.locationUri = "file://myhost/foo/bar/hoge.iso"
         identifier.circumstancesKeywords = ["いちご", "メロン"]
         identifier.contentsKeywords = ["動物", "植物"]
