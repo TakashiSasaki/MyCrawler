e0ae9fa8e9e3385b9fdd46eef8e5a533f56dac22
unknown <Takashi@.(none)>
2012-01-31 19:10:01 +0900

    Indentifier can have multiple metadata amd also one parent.

--- Identifier.py
+++ Identifier.py
@@ -7,8 +7,8 @@
 from datetime import datetime 
 from google.appengine.ext.db import URLProperty, StringListProperty, \
     ListProperty, BooleanProperty, ReferenceProperty, StringProperty, Model, \
-    SelfReferenceProperty
-from google.appengine.api.datastore import Key
+    SelfReferenceProperty, DateTimeProperty, GeoPt, GqlQuery
+from google.appengine.api.datastore import Key, DatastoreAdapter
 from GaeAdopter import Initialize
 from libobomb.Uuid import GetBase32Uuid
 from google.appengine.ext.db.polymodel import PolyModel
@@ -17,6 +17,14 @@ from unittest.case import TestCase
 import urllib2
 from urllib import pathname2url
 from urlparse import urlparse
+from furl.furl import furl
+from google.appengine.api.datastore_errors import BadValueError
+from google.appengine.api.apiproxy_stub_map import apiproxy
+from google.appengine.api import datastore
+from google.appengine.ext.db.stats import KindStat
+from GaeAdopter.GaeAdopter import GaeAdopter
+from google.appengine.ext.db.metadata import Kind
+from logging import getLogger, debug, DEBUG
 
 class WordsModel(PolyModel):
     words = StringListProperty()
@@ -38,16 +46,23 @@ def IsInstances(list_, class_):
             return False
     return True
 
+def TimeSpacePoint():
+    pass
+    
 class IdentifierModel(VersionedItemPolyModel):
     identifierString = StringProperty(required=True)
-    timeSpacePoints = ListProperty(Key, required=True)
-    labels = ReferenceProperty(LabelsModel)
+    fileLocations = StringListProperty()
+    fileLocationTimes  = ListProperty(datetime)
+    geoLocations = ListProperty(GeoPt)
+    geoLocationTimes = ListProperty(datetime)
+    labels = StringListProperty()
     circumstances = ListProperty(Key)
     contents = ListProperty(Key)
-    metadata = ListProperty(Key)
+    observedTimes = ListProperty(datetime)
+    metadataList = ListProperty(Key)
     #children = ListProperty(Key)
     #complete = BooleanProperty()
-    anchor = SelfReferenceProperty()
+    includeIn = SelfReferenceProperty()
     def setCircumstances(self, circumstances_):
         pass
 
@@ -57,9 +72,8 @@ class IdentifierModel(VersionedItemPolyModel):
 #    completeness = BooleanProperty()
 
 class Test(TestCase):
-    import logging
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    getLogger().setLevel(DEBUG)
+    Initialize("obomb")
     
     def setUp(self):
         pass
@@ -68,29 +82,32 @@ class Test(TestCase):
         pass
     
     def testSomething(self):
-        Initialize("obomb")
-        #owner = ObombUserModel(displayName="TestUser", email="testuser@example.com", lastAccessTime=datetime.now(), deleted=False)
-        #owner.put()
-        Initialize("obomb")
         identifier = IdentifierModel(lockBegins=datetime.now(), lockEnds=datetime.now(), identifierString="urn:uuid:" + GetBase32Uuid(),
                                      owner=22222, versionNumber=1111, locationString="http://exmaple.com/a/b/c"
-                                     , item=456)
+                                     , item=456, labels =["label1", "label2"])
         identifier.circumstancesKeywords = ["いちご", "メロン"]
         identifier.contentsKeywords = ["動物", "植物"]
         identifier.put()
         gql = IdentifierModel.gql("")
-        for x in gql.fetch(100):
-            assert isinstance(x, IdentifierModel)
-            print x
+        try:    
+            for x in gql.fetch(100):
+                assert isinstance(x, IdentifierModel)
+                print x
+        except BadValueError, e:
+            self.logger.error(str(e))
+        
+        q = GqlQuery("SELECT * FROM IdentifierModel")
+        self.logger.debug("SELECT * FROM IdentifierModel results %s items"% len(q))
 
     def testClassType(self):
         class A(object):
             pass
         alist = [A(), A(), A()]
         assert IsInstances(alist, A)
-        self.logger.debug(type(A))
+        self.logger.info(type(A))
     
     def testTraverse(self):
+        logger = getLogger().getChild("testTraverse")
         import os
         file_counter = 0
         dir_counter = 0
@@ -98,18 +115,24 @@ class Test(TestCase):
             for file in files:
                 file_counter += 1
                 path = os.path.abspath(os.path.join(root, file))
-                url = pathname2url(path)
-                parsed = urlparse(url)
-                self.logger.debug("file[" + str(file_counter) + "]" + parsed.geturl())
+                url = "file://localhost" + pathname2url(path)[2:]
+                logger.debug("file[" + str(file_counter) + "]" + url)
                 #identifier = IdentifierModel() 
             for dir in dirs:
                 dir_counter += 1
                 path = os.path.abspath(os.path.join(root, dir))
-                url = pathname2url(path)
-                parsed = urlparse(url)
-        
-                self.logger.debug("dir[" + str(dir_counter) + "]" + parsed.geturl())
-        
+                url = "file://localhost" + pathname2url(path)[2:]
+                logger.debug("dir[" + str(dir_counter) + "]" + url)
+            f = furl("http://a.example.com/path/http://b.example.com/xyz.html")
+            logger.debug(f.url)
+    
+    def testGetKinds(self):
+        logger = getLogger().getChild(__name__)
+        Initialize("obomb")
+        q = Kind.all()
+        for kind in q.fetch(100):
+            logger.info(kind.kind_name)
+    
 if __name__ == "__main__":
     class A(object):
         pass
