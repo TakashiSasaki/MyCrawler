cd70e244ac0b403e2c352f0d4724035712f1c1f0
Takashi SASAKI <takashi316@gmail.com>
2012-05-22 19:19:17 +0900

    main py runs demo_app in two ways.

---
+++ main.py
@@ -0,0 +1,46 @@
+'''
+Created on 2012/05/22
+
+@author: sasaki
+'''
+from __future__ import  print_function, unicode_literals
+from wsgiref.simple_server import demo_app
+from threading import Thread
+
+class WsgirefThread(Thread):
+    def __init__(self, app = demo_app):
+        Thread.__init__(self)
+        self.app = app
+        
+    def run(self):
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 8001, self.app)
+        httpd.serve_forever()
+    
+class PasteThread(Thread):
+    def __init__(self, app = demo_app):
+        Thread.__init__(self)
+        self.app = app
+        
+    def run(self):
+        from paste import httpserver
+        httpserver.serve(self.app, host='127.0.0.1', port='8002')
+
+import webbrowser
+
+if __name__ == "__main__":
+    while True:
+        print ("1: run demo_app by wsgiref on localhost:8001")
+        print ("2: run demo_app by paste on localhost:8001")
+        a = raw_input("3: exit >")
+        if a == "1":
+            t = WsgirefThread()
+            t.start()
+            webbrowser.open("http://localhost:8001/", autoraise=1)
+        elif a == "2":
+            t = PasteThread()
+            t.start()
+            webbrowser.open("http://localhost:8002/", autoraise=1)
+        elif a == "3":
+            print ("bye")
+            exit()
