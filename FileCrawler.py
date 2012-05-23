9a4020b6fc26fe4a512c2e96acdcd419d1d94421
unknown <sasaki@.(none)>
2012-05-23 22:05:06 +0900

    FileCrawler class supports thread.

--- FileCrawler.py
+++ FileCrawler.py
@@ -1,13 +1,17 @@
 from __future__ import unicode_literals, print_function
 from MyObject import MyObject
-import os
 import os.path
 import json
+import socket  
 from datetime import datetime
+from hashlib import sha1
+from threading import Thread
+from unittest import TestCase
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
 
 def _getHostName():
     try:
-        import socket  
         host_name = socket.gethostname()
         assert host_name is not None  
     except:
@@ -31,7 +35,6 @@ BUFFER_SIZE = 1024 * 1024
 
 def _calculateGitBlobHash(path, size):
     f = open(path, "rb")
-    from hashlib import sha1
     s = sha1()
     s.update("blob %s\0" % size)
     while True:
@@ -40,35 +43,40 @@ def _calculateGitBlobHash(path, size):
         s.update(b)
     return s.hexdigest()
 
-def crawlUnderTheDirectory(path, session):
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            p = root + os.path.sep + f
-            ap = os.path.abspath(p)
-            s = os.stat(ap)
-            file_info = FileInfo()
-            file_info.absolutePath = ap
-            file_info.st_size = s.st_size
-            file_info.st_mtime = s.st_mtime
-            file_info.st_ctime = s.st_ctime
-            file_info.gitBlobHash = _calculateGitBlobHash(ap, s.st_size)
-            print (file_info.toJson())
-            my_object = MyObject()
-            my_object.lastModified = datetime.fromtimestamp(file_info.st_mtime) # naive or aware?
-            my_object.lastSeen = datetime.now()
-            my_object.uri = "git:blob:" + file_info.gitBlobHash
-            my_object.url = "file://" + _getHostName() + "/" + file_info.absolutePath
-            session.add(my_object)
+class FileCrawler(Thread):
+    def __init__(self, path, sqlalchemy_session):
+        Thread.__init__(self)
+        self.path = path
+        self.sqlAlchemySession = sqlalchemy_session
+        pass
+    
+    def run(self):
+        for root, dirs, files in os.walk(self.path):
+            for f in files:
+                p = root + os.path.sep + f
+                ap = os.path.abspath(p)
+                s = os.stat(ap)
+                file_info = FileInfo()
+                file_info.absolutePath = ap
+                file_info.st_size = s.st_size
+                file_info.st_mtime = s.st_mtime
+                file_info.st_ctime = s.st_ctime
+                file_info.gitBlobHash = _calculateGitBlobHash(ap, s.st_size)
+                print (file_info.toJson())
+                my_object = MyObject()
+                my_object.lastModified = datetime.fromtimestamp(file_info.st_mtime) # naive or aware?
+                my_object.lastSeen = datetime.now()
+                my_object.uri = "git:blob:" + file_info.gitBlobHash
+                my_object.url = "file://" + _getHostName() + "/" + file_info.absolutePath
+                self.sqlAlchemySession.add(my_object)
 
-from unittest import TestCase
 class _Test(TestCase):
     def setUp(self):
-        from sqlalchemy import create_engine
         engine = create_engine("sqlite:///test3.sqlite", echo=True)
-        from sqlalchemy.orm.session import sessionmaker
         SessionClass = sessionmaker(bind=engine)
         self.session = SessionClass()
     
     def test1(self):
-        crawlUnderTheDirectory(".", self.session)
-        self.session.commit()
+        file_crawler = FileCrawler(".", self.session)
+        file_crawler.start()
+        file_crawler.join()
