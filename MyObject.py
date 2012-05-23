4b6b625207f03961dbb4e831b379fb8af33ff865
unknown <sasaki@.(none)>
2012-05-23 19:36:09 +0900

    class MemoMap and class MySession

--- MyObject.py
+++ MyObject.py
@@ -1,12 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Date, Integer, MetaData, create_engine
+from sqlalchemy.orm.session import sessionmaker
 metadata = MetaData()
 DeclarativeBase = declarative_base(metadata=metadata)
 
 class MyObject(DeclarativeBase):
     __tablename__ = "MyObject"
     id = Column(Integer, primary_key=True) #unique only on this database
-    agentId = Column(Integer) #MAC address can be used
     sessionId = Column(Integer) #identical for one session
     gitHash = Column(String()) #git style hash value
     uri = Column(String())
@@ -14,16 +14,33 @@ class MyObject(DeclarativeBase):
     lastModified = Column(Date()) #last modified datetime
     lastSeen = Column(Date()) #last seen datetime
     jsonString = Column(String()) #serialized data
-    memo0 = Column(String(),nullable=True)
-    memo1 = Column(String(),nullable=True)
-    memo2 = Column(String(),nullable=True)
-    memo3 = Column(String(),nullable=True)
-    memo4 = Column(String(),nullable=True)
-    memo5 = Column(String(),nullable=True)
-    memo6 = Column(String(),nullable=True)
-    memo7 = Column(String(),nullable=True)
-    memo8 = Column(String(),nullable=True)
-    memo0 = Column(String(),nullable=True)
+    memo0 = Column(String(), nullable=True)
+    memo1 = Column(String(), nullable=True)
+    memo2 = Column(String(), nullable=True)
+    memo3 = Column(String(), nullable=True)
+    memo4 = Column(String(), nullable=True)
+    memo5 = Column(String(), nullable=True)
+    memo6 = Column(String(), nullable=True)
+    memo7 = Column(String(), nullable=True)
+    memo8 = Column(String(), nullable=True)
+    memo9 = Column(String(), nullable=True)
+
+class MemoMap(DeclarativeBase):
+    __tablename__ = "MemoMap"
+    memoId = Column(Integer, primary_key=True)
+    memoName = Column(String(), nullable=False)
+    def __init__(self, memo_id, memo_name):
+        self.memoId = memo_id
+        self.memoName = memo_name
+    
+class MySession(DeclarativeBase):
+    __tablename__ = "Session"
+    sessionId = Column(Integer(), primary_key=True)
+    agentId = Column(Integer(), nullable=False) #MAC address can be used
+    beginDateTime = Column(Date(), nullable=False)
+    endDateTime = Column(Date(), nullable=False)
+    userName = Column(String(), nullable=False)
+    userDomain = Column(String(), nullable=False)
 
 if __name__ == "__main__":
     from uuid import getnode
@@ -32,3 +49,8 @@ if __name__ == "__main__":
     engine = create_engine("sqlite:///test3.sqlite", echo=True)
     metadata.create_all(engine)
 
+    SessionClass = sessionmaker(bind=engine)
+    session = SessionClass()
+    session.add(MemoMap(1,"one"))
+    session.commit()
+    
