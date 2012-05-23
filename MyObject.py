3a26169cc6d7f96ecf840e3c2387fec273b6b26e
unknown <sasaki@.(none)>
2012-05-23 19:19:04 +0900

    MyObject in 'declarative' way

--- MyObject.py
+++ MyObject.py
@@ -1,18 +1,34 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Date, Integer
-DeclarativeBase = declarative_base()
+from sqlalchemy import Column, String, Date, Integer, MetaData, create_engine
+metadata = MetaData()
+DeclarativeBase = declarative_base(metadata=metadata)
 
 class MyObject(DeclarativeBase):
     __tablename__ = "MyObject"
-    localId = Column(Integer, primary_key = True)
-    sessionId= Column(Integer)
-    gitHash = Column(String())
+    id = Column(Integer, primary_key=True) #unique only on this database
+    agentId = Column(Integer) #MAC address can be used
+    sessionId = Column(Integer) #identical for one session
+    gitHash = Column(String()) #git style hash value
     uri = Column(String())
     size = Column(Integer())
-    unchangedBegin = Column(Date())
-    unchangedLast = Column(Date())
-    jsonString = Column(String())
+    lastModified = Column(Date()) #last modified datetime
+    lastSeen = Column(Date()) #last seen datetime
+    jsonString = Column(String()) #serialized data
+    memo0 = Column(String(),nullable=True)
+    memo1 = Column(String(),nullable=True)
+    memo2 = Column(String(),nullable=True)
+    memo3 = Column(String(),nullable=True)
+    memo4 = Column(String(),nullable=True)
+    memo5 = Column(String(),nullable=True)
+    memo6 = Column(String(),nullable=True)
+    memo7 = Column(String(),nullable=True)
+    memo8 = Column(String(),nullable=True)
+    memo0 = Column(String(),nullable=True)
 
-from uuid import getnode
-node = getnode()
-print (hex(node))
+if __name__ == "__main__":
+    from uuid import getnode
+    node = getnode()
+    print (hex(node))
+    engine = create_engine("sqlite:///test3.sqlite", echo=True)
+    metadata.create_all(engine)
+
