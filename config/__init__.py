6882b9e311f7ee5851846b8e26a5c1e14112129c
Takashi SASAKI <takashi316@gmail.com>
2012-05-31 23:25:44 +0900

    engine for sqlalchemy is created in config

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,6 @@
+from __future__ import  print_function, unicode_literals
 import logging as _logging
 _logging.basicConfig(level=_logging.DEBUG)
 from unittest import TestCase, main
-from __future__ import  print_function, unicode_literals
+from sqlalchemy import create_engine as _create_engine
+engine = _create_engine("sqlite:///test3.sqlite", echo=True)
