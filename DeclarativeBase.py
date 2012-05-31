bac09ce1b045b1096b4ab09d36c0aebd67ec7c5d
Takashi SASAKI <takashi316@gmail.com>
2012-06-01 00:30:08 +0900

    DeclarativeBase class was moved to config

--- DeclarativeBase.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import  MetaData
-from sqlalchemy.ext.declarative import declarative_base as _declarative_base
-_metadata = MetaData()
-DeclarativeBase = _declarative_base(metadata=_metadata)
--- config/__init__.py
+++ config/__init__.py
@@ -4,3 +4,9 @@ _logging.basicConfig(level=_logging.DEBUG)
 from unittest import TestCase, main
 from sqlalchemy import create_engine as _create_engine
 engine = _create_engine("sqlite:///test3.sqlite", echo=True)
+from sqlalchemy.orm.session import sessionmaker as _sessionmaker
+Session = _sessionmaker(bind=engine)
+from sqlalchemy import  MetaData as _MetaData
+from sqlalchemy.ext.declarative import declarative_base as _declarative_base
+#_metadata = MetaData()
+DeclarativeBase = _declarative_base(metadata=_MetaData())
