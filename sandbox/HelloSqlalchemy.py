554a3e2afef2b0054e3144853f2d14f082724be3
Takashi SASAKI <takashi316@gmail.com>
2012-05-31 22:15:36 +0900

    sample code for sqlalchemy

---
+++ sandbox/HelloSqlalchemy.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.orm.session import sessionmaker
+from unittest import TestCase, main
+from logging import debug
+
+class User(object):
+    def __init__(self, name, fullname, password):
+        """SQLAlchemy does not require this method"""
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+        """SQLAlchemy does not require this method"""
+        return "<User('%s','%s','%s')>" % (self.name, self.fullname, self.password)
+
+class _Test(TestCase):
+    def setUp(self):
+        metadata = MetaData()
+        self.usersTable = Table('users', metadata,
+                            Column('id', Integer, primary_key=True),
+                            Column('name', String),
+                            Column('fullname', String),
+                            Column('password', String))
+        self.engine = create_engine("sqlite:///test2.sqlite", echo=True)
+        metadata.create_all(self.engine)
+        debug("message")
+        from sqlalchemy.orm import mapper
+        mapper(User, self.usersTable)
+
+    def testAddRecord(self):
+        ed_user = User('ed', "Ed Jones", "edpassword")
+        print (ed_user.fullname)
+        print (ed_user.password)
+        
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        session.add(ed_user)
+        session.commit()
+
+if __name__ == "__main__":
+    main()
