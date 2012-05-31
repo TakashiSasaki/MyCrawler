e549f1df1b0fef5052e5aaaa1a3bb57f10b50b4a
Takashi SASAKI <takashi316@gmail.com>
2012-05-31 22:10:46 +0900

    sample code for sqlalchemy was moved to sandbox.

--- MyCrawler.py
+++ MyCrawler.py
@@ -1,40 +1,6 @@
 from webapp2 import RequestHandler, WSGIApplication
 from wsgiref.simple_server import demo_app
 
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm.session import sessionmaker
-metadata = MetaData()
-users_table = Table('users', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('name', String),
-                    Column('fullname', String),
-                    Column('password', String))
-engine = create_engine("sqlite:///test2.sqlite", echo=True)
-metadata.create_all(engine)
-
-class User(object):
-    def __init__(self, name, fullname, password):
-        """SQLAlchemy dows not require this method"""
-        self.name = name
-        self.fullname = fullname
-        self.password = password
-
-    def __repr__(self):
-        """SQLAlchemy does not require this method"""
-        return "<User('%s','%s','%s')>" % (self.name, self.fullname, self.password)
-
-from sqlalchemy.orm import mapper
-mapper(User, users_table)
-
-ed_user = User('ed', "Ed Jones", "edpassword")
-print (ed_user.fullname)
-print (ed_user.id)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-session.add(ed_user)
-session.commit()
 
 class MyHandler(RequestHandler):
     def get(self):
