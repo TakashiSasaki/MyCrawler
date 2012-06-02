b82bef09a2e086a0e18db10678c205b7f97d577b
Takashi SASAKI <takashi316@gmail.com>
2012-06-02 13:36:19 +0900

    cascaded WSGI apps in App.py

---
+++ App.py
@@ -0,0 +1,29 @@
+from config import *
+from webapp2 import RequestHandler, WSGIApplication
+from wsgiref.simple_server import demo_app
+
+
+class _EnvironmentApp(WSGIApplication):
+    class _EnvironmentHandler(RequestHandler):
+        def get(self):
+            import os
+            self.response.out.write(os.environ)
+
+    def __init__(self):
+        WSGIApplication.__init__(self, [("/Environment", _EnvironmentApp._EnvironmentHandler),
+                                        ("/environment", _EnvironmentApp._EnvironmentHandler)],
+                                 debug=True, config=None)
+
+if __name__ == "__main__":
+    from paste.urlparser import StaticURLParser
+    html_app = StaticURLParser("html/")
+    css_app = StaticURLParser("css/")
+    js_app = StaticURLParser("js/")
+    
+    from paste.cascade import Cascade
+    cascaded_app = Cascade([html_app, css_app, js_app, _EnvironmentApp(), demo_app])
+    from lib.WsgiRunner import PasteThread
+    PasteThread(cascaded_app, 10523).start()
+
+    import webbrowser
+    webbrowser.open("http://localhost:10523/environment", autoraise=1)
